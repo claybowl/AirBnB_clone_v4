217585457b65b92f001edc421c65658c144837e4
Clayton Christian <claybowlproductions@gmail.com>
2022-12-08 18:47:34 -0600

    added uuid import

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -8,6 +8,7 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 app = Flask(__name__)
+import uuid
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
@@ -33,13 +34,13 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-    cache_id = uuid.uuid4()
+
 
     return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=cache_id)
+                           cache_id=uuid.uuid4())
 
 
 if __name__ == "__main__":
